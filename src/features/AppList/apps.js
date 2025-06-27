06f38cf1c376b111ee556de7611e630017ebde30
pjmk007 <pjmk007@gmail.com>
2025-06-27 16:10:04 +0900

    순서 변경

--- src/features/AppList/apps.js
+++ src/features/AppList/apps.js
@@ -1,17 +1,17 @@
 // 앱 목록을 정의하는 정적 데이터
 export const apps = [
-  {
-    id: 'wind-load',
-    name: '풍하중 산정기',
-    description: 'KDS 기준 풍하중 자동 계산기',
-    icon: '🌬️',
-    link: 'http://10.177.144.74:3001',
-  },
-  {
-    id: 'seismic-load',
-    name: '지진하중 산정기',
-    description: 'ASCE, KDS, Eurocode 지원',
-    icon: '🌎',
-    link: 'http://10.177.144.74:3002',
-  },
+    {
+        id: 'seismic-load',
+        name: '지진하중 산정기',
+        description: 'ASCE, KDS, Eurocode 지원',
+        icon: '🌎',
+        link: 'http://10.177.144.74:3001',
+    },
+    {
+      id: 'wind-load',
+      name: '풍하중 산정기',
+      description: 'KDS 기준 풍하중 자동 계산기',
+      icon: '🌬️',
+      link: 'http://10.177.144.74:3002',
+    },
 ];
